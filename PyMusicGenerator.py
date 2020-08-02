3562258172a102a85fb679244184c94f4b62a067
ATCtech <ankush4singh@gmail.com>
2020-08-02 12:10:31 +0530

    read from multiple files and save the generated lyrics

--- PyMusicGenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-from replacement import *
-
-Markov = {}
-
-word_list = get_words_list_from_file('./data/pkmn.txt')
-
-for word in word_list:
-    print(word)
--- word_handling.py
+++ word_handling.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 # VARIABLES
 puntuations = ['.',',','"',"'",'!','*',';','-','(',')']
 
+# FUNCTION TO REMOVE ALL PUNTUATION FROM A TEXT FILE
 def replace(file_location):
     try:
         with open(file_location,"r") as lyrics:
@@ -16,6 +19,7 @@ def replace(file_location):
     except Exception as e:
         print(e)
         
+# FUNCTION TO READ ALL TEXT FROM A TEXT FILE
 def read_lyrics_from_file(file_location_):
     replace(file_location_)
     lyrics_str=""
@@ -27,12 +31,15 @@ def read_lyrics_from_file(file_location_):
     except Exception as e:
         print(e)
         
-def get_words_list_from_file(file_loc):
+# THIS FUNCTION RETURNS AN ARRAY OF ALL WORDS PRESENT IN A TEXT FILE SEPERATED BY SPACES AND \n
+def get_words_from_file(file_loc):
+    
     try:
-        lyrics_lines_lst = read_lyrics_from_file(file_loc).split("\n")
-        lyrics_word_lst = []
-        for line in lyrics_lines_lst:
-            lyrics_word_lst += line.split()
-        return lyrics_word_lst
+        lyrics_lines_arr = np.array(read_lyrics_from_file(file_loc).split("\n"))
+        lyrics_word_arr = np.array([])
+        for line in lyrics_lines_arr:
+            lyrics_word_arr=np.append(lyrics_word_arr,np.array(line.split(" ")))
+        return lyrics_word_arr
     except Exception as e:
         print(e)
+        
