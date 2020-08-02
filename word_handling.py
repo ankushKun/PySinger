a493bbd5a7605d6a0ba98b647368661accd8413a
Priyanshu <67860164+Priyanshu79@users.noreply.github.com>
2020-08-02 13:07:29 +0530

    Update word_handling.py

--- word_handling.py
+++ word_handling.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # VARIABLES
-puntuations = ['.',',','"',"'",'!','*',';','-','(',')','—']
+puntuations = np.array(['.',',','"',"'",'!','*',';','-','(',')','—'])
 
 # FUNCTION TO REMOVE ALL PUNTUATION FROM A TEXT FILE
 def replace(file_location):
